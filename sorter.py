19c0e9967bfe8de08ea27a484264bc4c5fb818dd
Archais <aipt321@gmail.com>
2020-10-29 23:38:22 +0200

    Sorter reads a list of PDFs from the directory listed in the config file. It then iterates through the files searching them for SI number, customer and date of fiscalisation, before renaming and moving the invoices, creating files as needed. Files will not be moved if the listed attributes can not be found, and the reason will be outputted to the terminal.

--- sorter.py
+++ sorter.py
@@ -1,27 +1,62 @@
 import json
 import os
 import re
+from datetime import datetime
+from time import sleep
 
+import PyPDF2
 
 # Load config file
 with open("config.json") as conf:
     config = json.load(conf)
     source = config["reprint"]
     regex = config["regex"]
-    print(regex)
 
 
-# Locate files in Reprint folder
-invoices = []
-for root, dirs, files in os.walk(source):
-    print("Root: ", root)
-    print("Folders: ", dirs)
-    print("Files: ", files)
-    print()
-    for file in files:
-        if re.search(regex, file):
-            invoices.append(file)
-    if (root == source):
-        break
+while not sleep(10):
+    # Locate files in Reprint folder
+    invoices = []
+    for root, dirs, files in os.walk(source):
+        for file in files:
+            if re.search(regex["pdf"], file):
+                invoices.append(file)
+        if (root == source):
+            break
 
-print(invoices)
+    if not invoices:
+        print("No pending files.", end= "\n\n")
+        continue
+    print("Pending PDFs:", invoices, end= "\n\n")
+
+
+    # Get invoice info, rename pdf and move to correct folder
+    for file in invoices:
+        sourceFile = source + os.sep + file
+        with open(sourceFile, "rb") as invoice:
+            try:
+                pdf = PyPDF2.PdfFileReader(invoice)
+                docInfo = pdf.getDocumentInfo()
+                if not (invoiceNum := re.findall(regex["invoice number"], docInfo["/Title"])[0]):
+                    raise Exception("No invoice number")
+                print("Invoice:", invoiceNum)
+                text = pdf.getPage(0).extractText()
+                if not (customer := re.findall(regex["customer"], text)[0]):
+                    raise Exception("Unable to find customer")
+                print("Customer:", customer)
+                dateTime = docInfo[r"/ModDate"][2:-7]
+                if not dateTime.isnumeric() or len(dateTime) != 14:
+                    raise Exception("Bad datetime")
+                dateTime = datetime(int(dateTime[0:4]), int(dateTime[4:6]), int(dateTime[6:8]), int(dateTime[8:10]), int(dateTime[10:12]), int(dateTime[12:]))
+                print("Fiscalised:", dateTime.strftime(r"%d %B %Y %H:%M:%S"))
+                if not os.path.isdir(destination := source + os.sep + str(dateTime.year) + os.sep + dateTime.strftime("%B %Y")):
+                    print("Folder(s) not found- generating now.")
+                    os.makedirs(destination)
+                newName = f"{invoiceNum}_{customer}_{dateTime.strftime('%Y%m%d_%H%M%S')}.pdf"
+                destination += os.sep + newName
+                os.rename(sourceFile, destination)
+                print("Destination:", destination)
+                
+            except Exception as error:
+                print(error)
+                invoice.close()
+        print()
